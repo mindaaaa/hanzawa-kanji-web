9af55037914513fe3a6f38b85b79bc0fef8efafb
MinJeong Hong <147925385+mindaaaa@users.noreply.github.com>
2025-03-30 03:28:03 +0900

    feat(StudyMode): 초기 데이터 fetch 기능 연결

--- src/components/StudyMode.jsx
+++ src/components/StudyMode.jsx
@@ -1,12 +1,40 @@
-import kanjiList from '../data/kanji.json';
 import KanjiCard from './KanjiCard.jsx';
 import styles from '../css/StudyMode.module.css';
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 
 export default function StudyMode() {
+  const [items, setItems] = useState([]);
+  const [loading, setLoading] = useState(false);
+
+  useEffect(() => {
+    const fetchKanji = async () => {
+      setLoading(true);
+
+      try {
+        const res = await fetch(
+          'http://localhost:40324/api/v1/hanzawa-kanji?quizId=yyy'
+        );
+        const data = await res.json();
+
+        setItems(data.items);
+      } catch (error) {
+        console.error('데이터 불러오기 실패👀', error);
+      } finally {
+        setLoading(false);
+      }
+    };
+    fetchKanji();
+  }, []);
+  // 1. 상태 관리 (items, cursor, loading)
+  // 2. fetch 함수
+  // 3. observer 연결 함수
+  // 4. useEffect 초기 fetch
+  // 5. return (마지막 요소에 ref 연결)
   return (
     <div className={styles.list}>
-      {kanjiList.map((kanji) => (
+      {loading && <p>로딩 중...</p>}
+
+      {items.map((kanji) => (
         <KanjiCard key={kanji.id} kanji={kanji} />
       ))}
     </div>
