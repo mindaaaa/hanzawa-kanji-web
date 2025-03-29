814afcbe210c219511c33fed04e4d58d8c2f30cf
MinJeong Hong <147925385+mindaaaa@users.noreply.github.com>
2025-03-30 04:12:38 +0900

    feat(StudyMode): cursor 기반 fetch 구조 적용

--- src/components/StudyMode.jsx
+++ src/components/StudyMode.jsx
@@ -3,40 +3,56 @@ import styles from '../css/StudyMode.module.css';
 import React, { useEffect, useState } from 'react';
 
 export default function StudyMode() {
+  // 1. 상태 관리 (items, cursor, loading)
   const [items, setItems] = useState([]);
+  const [cursor, setCursor] = useState(null);
   const [loading, setLoading] = useState(false);
 
-  useEffect(() => {
-    const fetchKanji = async () => {
-      setLoading(true);
+  // const observer = useRef(); /* DOM 요소에 접근해야하므로 */
 
-      try {
-        const res = await fetch(
-          'http://localhost:40324/api/v1/hanzawa-kanji?quizId=yyy'
-        );
-        const data = await res.json();
-
-        setItems(data.items);
-      } catch (error) {
-        console.error('데이터 불러오기 실패👀', error);
-      } finally {
-        setLoading(false);
-      }
-    };
-    fetchKanji();
-  }, []);
-  // 1. 상태 관리 (items, cursor, loading)
   // 2. fetch 함수
+
+  const fetchKanji = async (nextCursor = null) => {
+    if (cursor === null && items.length === 2136) return;
+    if (loading) return;
+    setLoading(true);
+
+    try {
+      const base = 'http://localhost:40324/api/v1/hanzawa-kanji?quizId=yyy';
+      const url = nextCursor ? `${base}&cursor=${nextCursor}` : base;
+
+      const res = await fetch(url);
+      const data = await res.json();
+
+      setItems((prev) => {
+        const newItems = data.items.filter(
+          (newItem) => !prev.some((prevItem) => prevItem.id === newItem.id)
+        );
+        return [...prev, ...newItems];
+      });
+      setCursor(data.cursor);
+    } catch (error) {
+      console.error('데이터 불러오기 실패👀', error);
+      setItems([]);
+      setCursor(null);
+    } finally {
+      setLoading(false);
+    }
+  };
+
+  useEffect(() => {
+    fetchKanji(cursor);
+  }, []);
+
   // 3. observer 연결 함수
   // 4. useEffect 초기 fetch
   // 5. return (마지막 요소에 ref 연결)
   return (
     <div className={styles.list}>
-      {loading && <p>로딩 중...</p>}
-
       {items.map((kanji) => (
         <KanjiCard key={kanji.id} kanji={kanji} />
       ))}
+      {loading && <p>로딩 중...</p>}
     </div>
   );
 }
