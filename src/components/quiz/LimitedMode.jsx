1f9643c251803f9a08f1f9aa7f94ebd25394342a
MinJeong Hong <147925385+mindaaaa@users.noreply.github.com>
2025-04-11 12:37:11 +0900

    feat(LimitedMode): 문제 보기 출력하는 기능 구현

--- src/components/quiz/LimitedMode.jsx
+++ src/components/quiz/LimitedMode.jsx
@@ -1,4 +1,4 @@
-import React, { useState, useEffect, useRef } from 'react';
+import React, { useState, useEffect, useRef, useMemo } from 'react';
 import KanjiCard from '../../components/KanjiCard.jsx';
 import { buildApiUrl } from '../../utils/queryHelpers.js';
 
@@ -41,6 +41,58 @@ export default function LimitedMode() {
 
   const currentQuiz = quizList[quizIndex];
 
+  // 훈, 음 있나 체크하고 랜덤하게 뽑아주기
+  // TODO: 음, 훈 중복된 경우 처리
+  // function pickRandomType(kanji) {
+  //   const types = [];
+
+  //   if (kanji.korean.kun && kanji.korean.kun.length > 0) types.push('kunyomi');
+  //   if (kanji.korean.on && kanji.korean.on > 0) types.push('onyomi');
+
+  //   if (types.length === 0) return null;
+
+  //   const randomIndex = Math.floor(Math.random() * types.length);
+  //   return types[randomIndex];
+  // }
+
+  function getMeaningAndReading(kanji) {
+    const { korean } = kanji;
+    if (!korean || korean.length === 0) return '없음 / 없음';
+
+    const { kun, on } = korean[0];
+    return `${kun} / ${on}`;
+  }
+
+  function shuffle(array) {
+    const copied = [...array];
+
+    for (let i = copied.length - 1; i > 0; i--) {
+      let j = Math.floor(Math.random() * (i + 1));
+      [copied[i], copied[j]] = [copied[j], copied[i]];
+    }
+
+    return copied;
+  }
+
+  const correctAnswer = useMemo(() => {
+    if (!currentQuiz) return '';
+    return getMeaningAndReading(currentQuiz);
+  }, [currentQuiz]);
+
+  // const type = currentQuiz ? pickRandomType(currentQuiz) : null;
+
+  const allChoices = useMemo(() => {
+    if (!currentQuiz || quizList.length === 0) return [];
+
+    const choices = shuffle(
+      quizList.filter((item) => item.id !== currentQuiz.id)
+    )
+      .slice(0, 3)
+      .map((item) => getMeaningAndReading(item));
+
+    return shuffle([correctAnswer, ...choices]);
+  }, [currentQuiz, quizList]);
+
   return (
     <div style={{ textAlign: 'center', padding: '2rem' }}>
       {loading && <p>로딩 중...🐌</p>}
@@ -60,6 +112,15 @@ export default function LimitedMode() {
           />
         </div>
       )}
+
+      {allChoices.map((choice, index) => (
+        <button
+          key={index}
+          style={{ margin: '0.5rem', padding: '1rem 2rem', fontSize: '1.2rem' }}
+        >
+          {choice}
+        </button>
+      ))}
 
       <div style={{ marginTop: '1rem' }}>
         {flipped && quizIndex < quizList.length - 1 && (
